dd02a032c31d2f6be6f51179bf1e72656fd27f26
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-02 14:29:07 -0700

    Updated to load and refresh users, that are not already stored in swiftdata, from the API

--- Fetcher/UserImporter.swift
+++ Fetcher/UserImporter.swift
@@ -12,31 +12,33 @@ import SwiftData
 struct UserImporter {
     static func fetchData(context: ModelContext) async throws {
         let url: String = "https://www.hackingwithswift.com/samples/friendface.json"
-
-        var userDescriptor = FetchDescriptor<User>()
-        userDescriptor.fetchLimit = 1
         
-        let persistedUsers = try context.fetch(userDescriptor)
+        guard let url = URL(string: url) else {
+            throw URLError(.badURL)
+        }
         
-        if persistedUsers.isEmpty {
-            guard let url = URL(string: url) else {
-                throw URLError(.badURL)
-            }
-            
-            
-            
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            
-            let decoder = JSONDecoder()
-            
-            let (data, _) = try await URLSession.shared.data(for: request)
-            
-            let decoded = try decoder.decode([User].self, from: data)
-            
-            if !decoded.isEmpty {
-                decoded.forEach { context.insert($0) }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let decoder = JSONDecoder()
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse) // Handle unsuccessful response codes
+        }
+        
+        let decoded = try decoder.decode([User].self, from: data)
+        
+        for user in decoded {
+            if !userExists(with: user.id, in: context) {
+                context.insert(user) // Insert only if user doesn't exist
             }
         }
+    }
+    
+    static func userExists(with id: String, in context: ModelContext) -> Bool {
+        let userDescriptor = FetchDescriptor<User>(predicate: #Predicate { $0.id == id })
+        return try! !context.fetch(userDescriptor).isEmpty
     }
 }
